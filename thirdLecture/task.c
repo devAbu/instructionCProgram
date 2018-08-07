c5fc4c2fd58a23a4b1b60c71474ecb9f595caf4b
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-07 13:42:32 +0200

    third

---
+++ thirdLecture/task.c
@@ -0,0 +1,11 @@
+// ispisat "Dobrodasao na nasu applikaciju", za nastavak ukucaj 1
+// ako je ukuco bilo sta sem 1 izbaci ga iz programa
+// kada ukuca 1, pitati usera za ime, prezima 
+// pitati ga (po imenu) koliko ima godina
+// ako je mladji od 18 reci mu da je rpogram samo za osobe starije od 18 i izabcit ga iz programa
+// ako ima 18 + , pitati ga da li ide na fakultet
+//ako ne ide na fakultet, reci mu da je program samo za one koji idu na fakultet i izbacit ga iz programa
+// ako ide na fakultet, pitati ga koliko ima predmeta na trenutnom semestru
+// na osnovu broj predmeta, reci useru da unese odredjeni broj ocjena (ako je 6 predmeta 6 puta pitati usera da unese ocjenu - for loop)
+// izracunati prosjek tih ocjena, i izbaciti useru prosjek i reci mu da li je prosao ili ne
+// program gotov
---
+++ thirdLecture/task2.c
@@ -0,0 +1,4 @@
+//pitati usera za koliko dana zeli da izracuna trosak
+// na osnovu odgovora pitati usera da unese trosak tog dana (svaki dan = jedan trosak) - for loop
+// izracunati prosjek njegovog troska i reci mu da na dan prosjecno potrosi "prosjek"
+// izracunati zbir trosenja i reci useru koliko je potrosio u odredjenom broju dana
