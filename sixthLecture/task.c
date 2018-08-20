13fa1b5215aea6682ddb2c8f8704647dca76fca2
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-20 11:56:31 +0200

    first question

---
+++ sixthLecture/task.c
@@ -0,0 +1,13 @@
+// isprintat da je program fakulteta
+// pitati korisnika da li je stariji od 18 godina
+// ako je mladji nece moc nastaviti jer nije student
+// traziti od korisnika da unese ime
+// traziti od korisnika da unese prezime
+// pozelejti korisniku dobrodoslicu
+// pitati korisnika koji odsjek
+// pitati korisnika koja je godina na fakultetu
+// ako je godina fakulteta veca od 3 ili manja od 0 reci mu da unese ispravan broj (fakultet traje 3 godine)
+// pitati korisnika koliko ima predemta
+// reci mu da unese predmete i nek mu se izracuna prosjek -> array i for
+// ako je pao obavijestiti ga i reci mu da se mora vise trudit
+// ako je polozio cestitat mu i pozeljet mu srecu na sljedecoj godini (ako je prva godina, sretno u 2 godini)
